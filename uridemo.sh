1d1db5f619fcc96211b57b2a53dfa21534283557
Uri Alon <urialon1@gmail.com>
2017-02-02 16:23:57 +0200

    server version from pom3

--- uridemo.sh
+++ uridemo.sh
@@ -14,12 +14,12 @@ DATA_DIR=data
 BIN_DIR=bin
 SRC_DIR=src
 
-CBOW=1
+CBOW=0
 NEG=5
 HS=0
 SAMPLE=0
 MIN_COUNT=200
-PATHS_MIN_COUNT=200
+PATHS_MIN_COUNT=0
 ITER=5
 
 DESC=js${length}x${width}_cbow${CBOW}_hs${HS}_neg${NEG}_sample${SAMPLE}_mincount${MIN_COUNT}_pathsmincount${PATHS_MIN_COUNT}_iter${ITER}
@@ -42,7 +42,7 @@ cd ..
 
 pushd ${SRC_DIR} && make; popd
 
-time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow $CBOW -size 150 -window 1 -negative $NEG -hs $HS -sample $SAMPLE -threads 64 -binary 1 -min-count $MIN_COUNT -paths-min-count $PATHS_MIN_COUNT -iter $ITER #-save-vocab $VOCAB_FILE
+$BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow $CBOW -size 150 -window 1 -negative $NEG -hs $HS -sample $SAMPLE -threads 64 -binary 1 -min-count $MIN_COUNT -iter $ITER -save-vocab $VOCAB_FILE
 
   
 echo -----------------------------------------------------------------------------------------------------
