2f2da9a37ecacb5b6fab331910f75e4848a0ce38
Uri Alon <urialon1@gmail.com>
2017-02-04 10:48:00 +0200

    server script

--- uridemo-multi2.sh
+++ uridemo-multi2.sh
@@ -1,90 +1,259 @@
 #!/bin/bash
 if [ $# -lt 2 ]
     then
-        echo "Usage: uridemo.sh <length> <width>"
-        exit 1
+        #echo "Usage: uridemo.sh <length> <width>"
+        #exit 1
+        length=4
+        width=1
+else
+    length=$1
+    width=$2
 fi
-length=$1
-width=$2
 
 DATA_DIR=data
 BIN_DIR=bin
 SRC_DIR=src
 
-MODEL=jsnice_training_clean${length}x${width}
-TEXT_DATA=../UnuglifyJS/glove_models/$MODEL.txt
+CBOW=1
+NEG=5
+HS=0
+SAMPLE=0
+MIN_COUNT=200
+PATHS_MIN_COUNT=0
+ITER=5
+
+DESC=js${length}x${width}_cbow${CBOW}_hs${HS}_neg${NEG}_sample${SAMPLE}_mincount${MIN_COUNT}_pathsmincount${PATHS_MIN_COUNT}_iter${ITER}
+MODEL=js_cbow${CBOW}_hs${HS}_iter${ITER}
+TEXT_DATA=../UnuglifyJS/glove_models/jsnice_training_clean${length}x${width}.txt
 VOCAB_FILE=models/$MODEL/vocab.txt
 VECTOR_DATA=models/$MODEL/vectors.bin
 
 if [ ! -d "models" ]; then
   mkdir models
 fi
+cd models
+if [ ! -d "$MODEL" ]; then
+  mkdir $MODEL
+  cd $MODEL
+  echo $DESC > description.txt
+  cd ..
+fi
+cd ..
 
 pushd ${SRC_DIR} && make; popd
 
-MODEL=jsnice_training_clean${length}x${width}xcbow0hs0_neg10
-VECTOR_DATA=models/$MODEL/vectors.bin
-cd models
-if [ ! -d "$MODEL" ]; then
-  mkdir $MODEL
-  cp jsnice_training_clean4x1/vocab.txt $MODEL
-fi
-cd ..
-time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow 0 -size 150 -window 1 -negative 10 -hs 0 -sample 0 -threads 64 -binary 1 -min-count 200 -paths-min-count 0 -read-vocab $VOCAB_FILE
-MODEL=jsnice_training_clean${length}x${width}xcbow1hs0_neg10
-VECTOR_DATA=models/$MODEL/vectors.bin
-cd models
-if [ ! -d "$MODEL" ]; then
-  mkdir $MODEL
-  cp jsnice_training_clean4x1/vocab.txt $MODEL
-fi
-cd ..
-time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow 1 -size 150 -window 1 -negative 10 -hs 0 -sample 0 -threads 64 -binary 1 -min-count 200 -paths-min-count 0 -read-vocab $VOCAB_FILE
-MODEL=jsnice_training_clean${length}x${width}xcbow1hs1_neg10
-VECTOR_DATA=models/$MODEL/vectors.bin
-cd models
-if [ ! -d "$MODEL" ]; then
-  mkdir $MODEL
-  cp jsnice_training_clean4x1/vocab.txt $MODEL
-fi
-cd ..
-time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow 1 -size 150 -window 1 -negative 10 -hs 1 -sample 0 -threads 64 -binary 1 -min-count 200 -paths-min-count 0 -read-vocab $VOCAB_FILE
-MODEL=jsnice_training_clean${length}x${width}xcbow0hs1_sample
-VECTOR_DATA=models/$MODEL/vectors.bin
-cd models
-if [ ! -d "$MODEL" ]; then
-  mkdir $MODEL
-  cp jsnice_training_clean4x1/vocab.txt $MODEL
-fi
-cd ..
-time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow 0 -size 150 -window 1 -negative 5 -hs 1 -sample 1e-4 -threads 64 -binary 1 -min-count 200 -paths-min-count 0 -read-vocab $VOCAB_FILE
-MODEL=jsnice_training_clean${length}x${width}xcbow0hs1_sample_neg10
-VECTOR_DATA=models/$MODEL/vectors.bin
-cd models
-if [ ! -d "$MODEL" ]; then
-  mkdir $MODEL
-  cp jsnice_training_clean4x1/vocab.txt $MODEL
-fi
-cd ..
-time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow 0 -size 150 -window 1 -negative 10 -hs 1 -sample 1e-4 -threads 64 -binary 1 -min-count 200 -paths-min-count 0 -read-vocab $VOCAB_FILE
+time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow $CBOW -size 150 -window 1 -negative $NEG -hs $HS -sample $SAMPLE -threads 64 -binary 1 -min-count $MIN_COUNT -save-vocab $VOCAB_FILE
 
-MODEL=naive_jsnice_training_clean_cbow0_hs1
+CBOW=0
+NEG=5
+HS=0
+SAMPLE=0
+MIN_COUNT=200
+PATHS_MIN_COUNT=0
+ITER=5
+
+DESC=js${length}x${width}_cbow${CBOW}_hs${HS}_neg${NEG}_sample${SAMPLE}_mincount${MIN_COUNT}_pathsmincount${PATHS_MIN_COUNT}_iter${ITER}
+MODEL=js_cbow${CBOW}_hs${HS}_iter${ITER}
+TEXT_DATA=../UnuglifyJS/glove_models/jsnice_training_clean${length}x${width}.txt
+VOCAB_FILE=models/$MODEL/vocab.txt
+VECTOR_DATA=models/$MODEL/vectors.bin
+
+if [ ! -d "models" ]; then
+  mkdir models
+fi
 cd models
 if [ ! -d "$MODEL" ]; then
   mkdir $MODEL
+  cd $MODEL
+  echo $DESC > description.txt
+  cd ..
 fi
 cd ..
-time bin/word2vec -train ../jslex/training_data/jsnice_training_clean.txt -output models/$MODEL/vectors.bin -cbow 0 -size 150 -window 10 -negative 5 -hs 1 -sample 0 -threads 64 -binary 1 -min-count 10 -save-vocab ./models/$MODEL/vocab.txt
 
-MODEL=naive_jsnice_training_clean_cbow1_hs0
+pushd ${SRC_DIR} && make; popd
+
+time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow $CBOW -size 150 -window 1 -negative $NEG -hs $HS -sample $SAMPLE -threads 64 -binary 1 -min-count $MIN_COUNT -save-vocab $VOCAB_FILE
+
+CBOW=1
+NEG=5
+HS=1
+SAMPLE=0
+MIN_COUNT=200
+PATHS_MIN_COUNT=0
+ITER=5
+
+DESC=js${length}x${width}_cbow${CBOW}_hs${HS}_neg${NEG}_sample${SAMPLE}_mincount${MIN_COUNT}_pathsmincount${PATHS_MIN_COUNT}_iter${ITER}
+MODEL=js_cbow${CBOW}_hs${HS}_iter${ITER}
+TEXT_DATA=../UnuglifyJS/glove_models/jsnice_training_clean${length}x${width}.txt
+VOCAB_FILE=models/$MODEL/vocab.txt
+VECTOR_DATA=models/$MODEL/vectors.bin
+
+if [ ! -d "models" ]; then
+  mkdir models
+fi
 cd models
 if [ ! -d "$MODEL" ]; then
   mkdir $MODEL
+  cd $MODEL
+  echo $DESC > description.txt
+  cd ..
 fi
 cd ..
-time bin/word2vec -train ../jslex/training_data/jsnice_training_clean.txt -output models/$MODEL/vectors.bin -cbow 1 -size 150 -window 10 -negative 5 -hs 0 -sample 0 -threads 64 -binary 1 -min-count 10 -save-vocab ./models/$MODEL/vocab.txt
-  
-#echo -----------------------------------------------------------------------------------------------------
-#echo -- distance...
 
-#$BIN_DIR/distance $VECTOR_DATA
+pushd ${SRC_DIR} && make; popd
+
+time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow $CBOW -size 150 -window 1 -negative $NEG -hs $HS -sample $SAMPLE -threads 64 -binary 1 -min-count $MIN_COUNT -save-vocab $VOCAB_FILE
+
+CBOW=0
+NEG=5
+HS=1
+SAMPLE=0
+MIN_COUNT=200
+PATHS_MIN_COUNT=0
+ITER=5
+
+DESC=js${length}x${width}_cbow${CBOW}_hs${HS}_neg${NEG}_sample${SAMPLE}_mincount${MIN_COUNT}_pathsmincount${PATHS_MIN_COUNT}_iter${ITER}
+MODEL=js_cbow${CBOW}_hs${HS}_iter${ITER}
+TEXT_DATA=../UnuglifyJS/glove_models/jsnice_training_clean${length}x${width}.txt
+VOCAB_FILE=models/$MODEL/vocab.txt
+VECTOR_DATA=models/$MODEL/vectors.bin
+
+if [ ! -d "models" ]; then
+  mkdir models
+fi
+cd models
+if [ ! -d "$MODEL" ]; then
+  mkdir $MODEL
+  cd $MODEL
+  echo $DESC > description.txt
+  cd ..
+fi
+cd ..
+
+pushd ${SRC_DIR} && make; popd
+
+time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow $CBOW -size 150 -window 1 -negative $NEG -hs $HS -sample $SAMPLE -threads 64 -binary 1 -min-count $MIN_COUNT -save-vocab $VOCAB_FILE
+
+##########################################################################################################
+# now 10 iterations
+
+CBOW=1
+NEG=5
+HS=0
+SAMPLE=0
+MIN_COUNT=200
+PATHS_MIN_COUNT=0
+ITER=10
+
+DESC=js${length}x${width}_cbow${CBOW}_hs${HS}_neg${NEG}_sample${SAMPLE}_mincount${MIN_COUNT}_pathsmincount${PATHS_MIN_COUNT}_iter${ITER}
+MODEL=js_cbow${CBOW}_hs${HS}_iter${ITER}
+TEXT_DATA=../UnuglifyJS/glove_models/jsnice_training_clean${length}x${width}.txt
+VOCAB_FILE=models/$MODEL/vocab.txt
+VECTOR_DATA=models/$MODEL/vectors.bin
+
+if [ ! -d "models" ]; then
+  mkdir models
+fi
+cd models
+if [ ! -d "$MODEL" ]; then
+  mkdir $MODEL
+  cd $MODEL
+  echo $DESC > description.txt
+  cd ..
+fi
+cd ..
+
+pushd ${SRC_DIR} && make; popd
+
+time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow $CBOW -size 150 -window 1 -negative $NEG -hs $HS -sample $SAMPLE -threads 64 -binary 1 -min-count $MIN_COUNT -save-vocab $VOCAB_FILE
+
+CBOW=0
+NEG=5
+HS=0
+SAMPLE=0
+MIN_COUNT=200
+PATHS_MIN_COUNT=0
+ITER=10
+
+DESC=js${length}x${width}_cbow${CBOW}_hs${HS}_neg${NEG}_sample${SAMPLE}_mincount${MIN_COUNT}_pathsmincount${PATHS_MIN_COUNT}_iter${ITER}
+MODEL=js_cbow${CBOW}_hs${HS}_iter${ITER}
+TEXT_DATA=../UnuglifyJS/glove_models/jsnice_training_clean${length}x${width}.txt
+VOCAB_FILE=models/$MODEL/vocab.txt
+VECTOR_DATA=models/$MODEL/vectors.bin
+
+if [ ! -d "models" ]; then
+  mkdir models
+fi
+cd models
+if [ ! -d "$MODEL" ]; then
+  mkdir $MODEL
+  cd $MODEL
+  echo $DESC > description.txt
+  cd ..
+fi
+cd ..
+
+pushd ${SRC_DIR} && make; popd
+
+time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow $CBOW -size 150 -window 1 -negative $NEG -hs $HS -sample $SAMPLE -threads 64 -binary 1 -min-count $MIN_COUNT -save-vocab $VOCAB_FILE
+
+CBOW=1
+NEG=5
+HS=1
+SAMPLE=0
+MIN_COUNT=200
+PATHS_MIN_COUNT=0
+ITER=10
+
+DESC=js${length}x${width}_cbow${CBOW}_hs${HS}_neg${NEG}_sample${SAMPLE}_mincount${MIN_COUNT}_pathsmincount${PATHS_MIN_COUNT}_iter${ITER}
+MODEL=js_cbow${CBOW}_hs${HS}_iter${ITER}
+TEXT_DATA=../UnuglifyJS/glove_models/jsnice_training_clean${length}x${width}.txt
+VOCAB_FILE=models/$MODEL/vocab.txt
+VECTOR_DATA=models/$MODEL/vectors.bin
+
+if [ ! -d "models" ]; then
+  mkdir models
+fi
+cd models
+if [ ! -d "$MODEL" ]; then
+  mkdir $MODEL
+  cd $MODEL
+  echo $DESC > description.txt
+  cd ..
+fi
+cd ..
+
+pushd ${SRC_DIR} && make; popd
+
+time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow $CBOW -size 150 -window 1 -negative $NEG -hs $HS -sample $SAMPLE -threads 64 -binary 1 -min-count $MIN_COUNT -save-vocab $VOCAB_FILE
+
+CBOW=0
+NEG=5
+HS=1
+SAMPLE=0
+MIN_COUNT=200
+PATHS_MIN_COUNT=0
+ITER=10
+
+DESC=js${length}x${width}_cbow${CBOW}_hs${HS}_neg${NEG}_sample${SAMPLE}_mincount${MIN_COUNT}_pathsmincount${PATHS_MIN_COUNT}_iter${ITER}
+MODEL=js_cbow${CBOW}_hs${HS}_iter${ITER}
+TEXT_DATA=../UnuglifyJS/glove_models/jsnice_training_clean${length}x${width}.txt
+VOCAB_FILE=models/$MODEL/vocab.txt
+VECTOR_DATA=models/$MODEL/vectors.bin
+
+if [ ! -d "models" ]; then
+  mkdir models
+fi
+cd models
+if [ ! -d "$MODEL" ]; then
+  mkdir $MODEL
+  cd $MODEL
+  echo $DESC > description.txt
+  cd ..
+fi
+cd ..
+
+pushd ${SRC_DIR} && make; popd
+
+time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow $CBOW -size 150 -window 1 -negative $NEG -hs $HS -sample $SAMPLE -threads 64 -binary 1 -min-count $MIN_COUNT -save-vocab $VOCAB_FILE
+
